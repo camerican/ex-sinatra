baf642342303074de0d3fb8144b6fb89a61ebe39
Cam Crews <camcrews@gmail.com>
2016-10-06 17:23:54 -0400

    Menu and Layout Examples

--- main.rb
+++ main.rb
@@ -1,33 +1,30 @@
 require 'sinatra'
 
 get '/' do
+  @menu = [
+    {:page => 'Home', :href => '/'},
+    {page: 'About', href: '/about'},
+    {page: 'Shelter', href: '/pound'}
+  ]
+
   @secret_number = 47
   @class = ["Arnold","Izza","Miguel","Alberto","John","Brian","Yerlan","Bryce","Emma Jean","Skylar","Mike","George","Kevin","Danna","William"]
   erb :home
 end
 
 get '/about' do
-<<-MYHTMLCONTENT
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>About Page</title>
-</head>
-<body>
-  <h1>About Page</h1>
-  <form method="POST" action="/about">
-  <label>Username</label><input name="username" placeholder="Enter your name" /> <input type="submit" value="Enter" />
-  </form>
-</body>
-</html>
-MYHTMLCONTENT
+  erb :about
 end
 
 post '/about' do
-  "Your name is #{params[:username]}"
+  @message = "Your name is #{params[:username]}"
+  erb :about
 end
 
+get '/pound' do
+  @dogs = ['dog1.jpg','dog2.jpg','dog3.jpg','dog4.jpg']
+  erb :pound
+end
 
 
 
